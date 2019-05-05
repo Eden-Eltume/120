6e395bf26c354f6412988ee3f105fc7c03ed359e
Eden Eltume <IEltume@gmail.com>
2019-05-05 12:02:54 -0400

    AI offense, defense and scoreboard

--- Lesson_5_Slightly_larger_OO_Programs/oo_ttt_bonus_features.rb
+++ Lesson_5_Slightly_larger_OO_Programs/oo_ttt_bonus_features.rb
@@ -1,4 +1,8 @@
+require 'pry-byebug'
+
 class Board
+  attr_accessor :squares
+
   WINNING_LINES = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] + # rows
                   [[1, 4, 7], [2, 5, 8], [3, 6, 9]] + # cols
                   [[1, 5, 9], [3, 5, 7]] # diagonals
@@ -22,6 +26,25 @@ class Board
 
   def someone_won?
     !!winning_marker
+  end
+
+  def at_risk_square(marker)
+    final_square = nil
+    WINNING_LINES.each do |line|
+      squares = @squares.values_at(*line)
+      if win_is_imminent?(squares, marker)
+        line.each do |square|
+          if @squares[square].unmarked?
+            final_square = square
+          end
+        end
+      end
+    end
+    final_square
+  end
+
+  def unmarked_middle?
+    @squares[5].unmarked?
   end
 
   # returns winning marker or nil
@@ -62,6 +85,11 @@ class Board
     return false if markers.size != 3
     markers.min == markers.max
   end
+
+  def win_is_imminent?(squares, player_marker)
+    squares.select { |square| square.marker == player_marker }.count == 2 &&
+    squares.select(&:unmarked?).size == 1
+  end
 end
 
 class Square
@@ -94,18 +122,59 @@ class Player
   end
 end
 
+class Scoreboard
+  WINNING_SCORE = 3
+
+  attr_reader :human_score, :computer_score, :ties
+
+  def initialize
+    @human_score = 0
+    @computer_score = 0
+    @ties = 0
+  end
+
+  def display_result
+    puts ""
+    puts "-" * 10 + "Scoreboard" + "-" * 10
+    puts "You:#{human_score}  " + "Computer:#{computer_score} " + " Ties:#{ties}  "
+    puts "-" * 15 + "-" * 15
+    puts ""
+  end 
+
+  def update(winning_marker)
+    if winning_marker == TTTGame::HUMAN_MARKER
+      @human_score += 1
+    elsif  winning_marker == TTTGame::COMPUTER_MARKER
+      @computer_score += 1
+    else
+      @ties  += 1
+    end
+  end
+
+  def winner?
+    if @human_score > 1 && @human_score == WINNING_SCORE
+      true
+    elsif @computer_score > 1 && @computer_score== WINNING_SCORE
+      true
+    else
+      false  
+    end
+  end
+end
+
 class TTTGame
   HUMAN_MARKER = "X"
   COMPUTER_MARKER = "O"
   FIRST_TO_MOVE = HUMAN_MARKER
 
-  attr_reader :board, :human, :computer
+  attr_reader :board, :human, :computer, :scoreboard
 
   def initialize
     @board = Board.new
     @human = Player.new(HUMAN_MARKER)
     @computer = Player.new(COMPUTER_MARKER)
     @current_marker = FIRST_TO_MOVE
+    @scoreboard = Scoreboard.new
   end
 
   def play
@@ -120,13 +189,22 @@ class TTTGame
         break if board.someone_won? || board.full?
         clear_screen_and_display_board if humans_turn?
       end
+      update_score
       display_result
-      break unless play_again?
+      break if scoreboard.winner? || !play_again?
       reset_game
       display_play_again_message
     end
 
     display_goodbye_message
+  end
+
+  def update_score
+    scoreboard.update(board.winning_marker)
+  end
+
+  def display_score
+    scoreboard.display_result
   end
 
   private
@@ -179,6 +257,30 @@ class TTTGame
   end
 
   def computer_moves
+    if board.at_risk_square(computer.marker)
+      attack_at_risk_square
+    elsif board.at_risk_square(human.marker)
+      defend_at_risk_square
+    elsif board.unmarked_middle?
+      choose_middle_square
+    else
+      choose_random_square
+    end 
+  end
+
+  def attack_at_risk_square
+    board[board.at_risk_square(computer.marker)] = computer.marker
+  end
+
+  def defend_at_risk_square
+    board[board.at_risk_square(human.marker)] = computer.marker
+  end
+
+  def choose_middle_square
+    board[5] = computer.marker
+  end
+
+  def choose_random_square
     board[board.unmarked_keys.sample] = computer.marker
   end
 
@@ -194,12 +296,13 @@ class TTTGame
   end
 
   def clear_screen_and_display_board
+    display_score
     clear_screen
     display_board
   end
 
   def display_result
-    display_board
+    display_score
     case board.winning_marker
     when HUMAN_MARKER
       puts "You won!"
